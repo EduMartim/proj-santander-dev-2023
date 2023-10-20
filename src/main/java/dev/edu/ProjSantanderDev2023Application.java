f1275cde21caff06d0cfc98c92e89944853e707b
edumartim <edumartim@gmail.com>
2023-10-20 16:18:06 -0300

    Initial commit

---
+++ src/main/java/dev/edu/ProjSantanderDev2023Application.java
@@ -0,0 +1,13 @@
+package dev.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjSantanderDev2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjSantanderDev2023Application.class, args);
+	}
+
+}
